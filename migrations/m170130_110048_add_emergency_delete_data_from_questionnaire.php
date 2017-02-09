2b10f088c7a17b01cc1a5fbdcb3a471343f406fa
yzavyalo <yzavyalo@cs.karelia.ru>
2017-02-09 09:24:30 +0300

    without drop data

--- migrations/m170130_110048_add_emergency_delete_data_from_questionnaire.php
+++ migrations/m170130_110048_add_emergency_delete_data_from_questionnaire.php
@@ -7,12 +7,12 @@ class m170130_110048_add_emergency_delete_data_from_questionnaire extends Migrat
     public function up()
     {
         $this->addColumn('questionnaire', 'emergency', $this->boolean()->notNull());
-        $this->dropColumn('questionnaire', 'data');
+        //$this->dropColumn('questionnaire', 'data');
     }
 
     public function down()
     {
         $this->dropColumn('questionnaire', 'emergency');
-        $this->addColumn('questionnaire', 'data', $this->text()->notNull());
+        //$this->addColumn('questionnaire', 'data', $this->text()->notNull());
     }
 }
