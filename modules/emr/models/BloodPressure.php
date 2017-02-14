bd1d087b9eff03b1e4c783662cf0bc8a2d2f554f
yzavyalo <yzavyalo@cs.karelia.ru>
2017-02-14 14:12:00 +0300

    fix bpdiary

--- modules/emr/models/BloodPressure.php
+++ modules/emr/models/BloodPressure.php
@@ -26,6 +26,54 @@ class BloodPressure extends ActiveRecord
             ['diastolic', 'integer']
         );
     }
+    
+        /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_at',
+                'updatedAtAttribute' => false,
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+    
+    
+        /**
+     * @inheritdoc
+     */
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->patient_id = \Yii::$app->user->identity->patient->id;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        if (!is_null($this->_responds)) {
+            $responds = $this->getRespondFactory()->createListFromData($this->_responds);
+
+            foreach ($responds as $respond) {
+                $respond->link('bloodpressure', $this);
+            }
+        }
+    }
 
     /**
      * @return \yii\db\ActiveQuery
